398e3c4c0939b221b615dcedfc94c210f99b4115
Da-zeros <daniromanpt@gmail.com>
2022-06-22 16:59:15 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+      this.length = this.items.length;
+      this.items = this.items.sort( function(a, b) {
+        if(a >= b) {
+          return 1;
+        } else if (a < b) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+  }
+    
+  get(pos) {
+    if(this.items[pos]!== undefined)return this.items[pos];
+    else throw new Error ("OutOfBounds");
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.items.length -1];
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
-  min() {}
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+    } else {
+      return this.items.reduce( function(a, b) {
+        return a + b;
+      }, 0);
+    }
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((a, b) => a + b, 0) / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
